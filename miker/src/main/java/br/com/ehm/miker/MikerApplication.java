d54a39fee0e8f0a233fca6984efca4c319456a2e
liliane da silva <hoshinoehm@gmail.com>
2023-01-08 23:34:41 -0300

    First steps in java with spring boot 3

---
+++ miker/src/main/java/br/com/ehm/miker/MikerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ehm.miker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikerApplication.class, args);
+	}
+
+}
---
+++ miker/src/test/java/br/com/ehm/miker/MikerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ehm.miker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
